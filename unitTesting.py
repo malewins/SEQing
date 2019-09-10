93578686aef47fea6fb089c546fd4db6a85705fd
Yannik Bramkamp <ybramkamp@techfak.uni-bielefeld.de>
2019-09-10 19:28:48 +0200

    #60, added more tests

--- unitTesting.py
+++ unitTesting.py
@@ -10,20 +10,38 @@ class TestDashboard(unittest.TestCase):
         self.maxDiff = None
         null = None
         for test in ['svg', 'png']:
-            self.assertEqual(eval(db.changeFormatiCLIP(test)), {'response': {'props': {'config':{'toImageButtonOptions' : {'filename' : 'iCLIP', 'width' : None,
+            self.assertEqual(eval(db.changeFormatiCLIP(test)), 
+                {'response': {'props': {'config':{'toImageButtonOptions' : {'filename' : 'iCLIP', 'width' : None,
                 'scale' : 1.0, 'height' : None, 'format' : test} }}}})
   
     def testFormatChangeRNA(self):
         self.maxDiff = None
         null = None
         for test in ['svg', 'png']:
-            self.assertEqual(eval(db.changeFormatRNA(test)), {'response': {'props': {'config':{'toImageButtonOptions' : {'filename' : 'RNA', 'width' : None,
+            self.assertEqual(eval(db.changeFormatRNA(test)), 
+                {'response': {'props': {'config':{'toImageButtonOptions' : {'filename' : 'RNA', 'width' : None,
                 'scale' : 1.0, 'height' : None, 'format' : test} }}}})
+    
+    def testChangeLegendSpacing(self):
+        self.maxDiff = None
+        for test in [0, 0.1, 0.2, 0.3]:
+            self. assertEqual(eval(db.changeLegendSpacing(test)),  {'response': {'props': {'children':str(test)}}} )
+    
+    def testShowHelpPopup(self):
+        self.maxDiff = None
+        null = None
+        testVals = [((0,1),{'response': {'props': {'style': {"display": "none"}}}}),
+                    ((1,0),{'response': {'props': {'style': {"display": "block"}}}}),
+                    ((1,1),{'response': {'props': {'style': {"display": "none"}}}})]
+        for test in testVals:
+            self.assertEqual(eval(db.showHelpPopup(test[0][0],test[0][1])),test[1])
     
     def testPreviewColorEvent(self):
         self.maxDiff = None
-        self.assertEqual(eval(db.previewColorEvent(23, None, 66)), {'response': {'props': {'style': {'backgroundColor': 'rgb(255, 255, 255)', 'color': 'rgb(255, 255, 255)'}}}})
-        self.assertEqual(eval(db.previewColorEvent(23, 48, 66)), {'response': {'props': {'style': {'backgroundColor': 'rgb(23,48,66)', 'color': 'rgb(23,48,66)'}}}})
+        self.assertEqual(eval(db.previewColorEvent(23, None, 66)),
+            {'response': {'props': {'style': {'backgroundColor': 'rgb(255, 255, 255)', 'color': 'rgb(255, 255, 255)'}}}})
+        self.assertEqual(eval(db.previewColorEvent(23, 48, 66)), 
+            {'response': {'props': {'style': {'backgroundColor': 'rgb(23,48,66)', 'color': 'rgb(23,48,66)'}}}})
         
     def testOverlap(self):
         self.assertTrue(db.overlap((1,4),(2,7)))
